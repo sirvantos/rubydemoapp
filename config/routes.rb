a9dde2e04363d15cf0b6756f08ec013b402d4741
Vladimir <sirvantos@gmail.com>
2014-05-07 18:14:36 +0400

    Added user controller skeletone

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Demoapp::Application.routes.draw do
+	get "users/new"
 	root to: 'static_pages#home'
 
-  	match '/help',		to: 'static_pages#help',	via: 'get'
+	match '/signup',	to: 'users#new',			via: 'get'
+	match '/help',		to: 'static_pages#help',	via: 'get'
 	match '/about',		to: 'static_pages#about',	via: 'get'
 	match '/contact',	to: 'static_pages#contact',	via: 'get'
 	resources :microposts
