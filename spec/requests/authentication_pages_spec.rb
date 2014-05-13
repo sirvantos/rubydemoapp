dc7c4a9cce2099a8d0198402814c11b3ca97e6cc
Vladimir <sirvantos@gmail.com>
2014-05-13 13:17:09 +0400

    Added session controller skeletone

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "AuthenticationPages" do
+	subject { page }
+
+	describe "sign in" do
+		before { visit signin_path }
+
+		it { should have_content("Sign in") }
+		it { should have_title("Sign in") }
+	end
+end
