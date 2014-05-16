ba18f65a55b9727abd68e94179982c6ad8dde6c8
Vladimir <sirvantos@gmail.com>
2014-05-16 18:35:25 +0400

    Added updating users + authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, only: [:edit, :update]
+	before_action :correct_user,   only: [:edit, :update]
+
 	def show
 		@user = User.find(params[:id])
 	end
 
 	def new
 		@user = User.new
+	end
+
+	def edit
+	end
+
+	def update
+		if @user.update_attributes(user_params)
+			flash[:success] = "Your data has been changed!"
+			redirect_to @user
+		else
+			render 'edit'
+		end
 	end
 
 	def create
@@ -21,5 +36,18 @@ class UsersController < ApplicationController
 	private
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		end
+
+		# Before filters
+		def signed_in_user
+			unless signed_in?
+				store_location
+				redirect_to signin_url, :flash => { :warning => "Please sign in" } unless signed_in?
+			end
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_url) unless current_user?(@user)
 		end
 end
