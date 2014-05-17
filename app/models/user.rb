48f45863ee85e2ac0f73f4fa80fc6a427a8dfe1b
Vladimir <sirvantos@gmail.com>
2014-05-17 16:00:00 +0400

    Added users microposts associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+	has_many :microposts, dependent: :destroy
+
 	validates :name, presence: true, length: { maximum: 64 }
 	validates :email, presence: true, length: { maximum: 128 }, format: {with: VALID_EMAIL_REGEX},
 			  uniqueness: { case_sensitive: false }
