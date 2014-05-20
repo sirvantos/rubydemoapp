fd8b164782f506c5b2c62d5d1a0db2af5a7403b9
Vladimir <sirvantos@gmail.com>
2014-05-20 19:18:14 +0400

    Added microposts delete link

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -55,6 +55,18 @@ describe "AuthenticationPages" do
 				it { should_not have_link('Profile', href: user_path(user)) }
 				it { should_not have_link('Settings', href: edit_user_path(user)) }
 			end
+
+			describe "in the Microposts controller" do
+				describe "submitting to the create action" do
+					before { post microposts_path }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+
+				describe "submitting to the destroy action" do
+					before { delete micropost_path(FactoryGirl.create(:micropost)) }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+			end
 		end
 
 		describe "as wrong user" do
